3e7c793e7a97001b5a2360953695e6e322002edc
sakthidaran3112 <sakthidaran3112@gmail.com>
2023-03-08 20:01:56 +0100

    Updated the UI

--- app/src/main/java/com/example/chat_bot/Activities/HomePage/ChatFragment.kt
+++ app/src/main/java/com/example/chat_bot/Activities/HomePage/ChatFragment.kt
@@ -19,15 +19,11 @@ import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.Toast
-import androidx.annotation.RequiresApi
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.databinding.DataBindingUtil.setContentView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chat_bot.Activities.QuizActivity
